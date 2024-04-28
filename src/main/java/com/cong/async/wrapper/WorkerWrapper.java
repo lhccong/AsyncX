2b6a9bf535b66d0727aa802512f8edc019830560
lhccong <771901874@qq.com>
2024-04-28 14:30:54 +0800

    feat: 🚀新增总控制台超时，停止所有任务方法、新增快速失败

--- src/main/java/com/cong/async/wrapper/WorkerWrapper.java
+++ src/main/java/com/cong/async/wrapper/WorkerWrapper.java
@@ -1,10 +1,16 @@
 package com.cong.async.wrapper;
 
+import com.cong.async.callback.DefaultCallback;
 import com.cong.async.callback.ICallback;
 import com.cong.async.callback.IWorker;
 import com.cong.async.worker.DependWrapper;
+import com.cong.async.worker.ResultState;
+import com.cong.async.worker.WorkResult;
 
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 工作器包装器(对每个worker及callback进行包装，一对一)
@@ -51,4 +57,123 @@ public class WorkerWrapper<T, V> {
      * 1、2执行完毕后才能执行3
      */
     private List<DependWrapper> dependWrappers;
+
+    /**
+     * 标记该事件是否已经被处理过了，譬如已经超时返回false了，后续rpc又收到返回值了，则不再二次回调
+     * 经试验,volatile并不能保证"同一毫秒"内,多线程对该值的修改和拉取
+     * <p>
+     * 1-finish, 2-error, 3-working
+     */
+    private AtomicInteger state = new AtomicInteger(0);
+
+    /**
+     * 该map存放所有wrapper的id和wrapper映射
+     */
+    private Map<String, WorkerWrapper> forParamUseWrappers;
+
+    /**
+     * 也是个钩子变量，用来存临时的结果
+     */
+    private volatile WorkResult<V> workResult = WorkResult.defaultResult();
+
+    /**
+     * 是否在执行自己前，去校验nextWrapper的执行结果<p>
+     * 注意，该属性仅在nextWrapper数量<=1时有效，>1时的情况是不存在的
+     */
+    private volatile boolean needCheckNextWrapperResult = true;
+
+    private WorkerWrapper(String id, IWorker<T, V> worker, T param, ICallback<T, V> callback) {
+        if (worker == null) {
+            throw new NullPointerException("async.worker is null");
+        }
+        this.worker = worker;
+        this.param = param;
+        this.id = id;
+        //允许不设置回调
+        if (callback == null) {
+            callback = new DefaultCallback<>();
+        }
+        this.callback = callback;
+    }
+    /**
+     * 开始工作
+     * fromWrapper代表这次work是由哪个上游wrapper发起的
+     */
+    private void work(ExecutorService executorService, WorkerWrapper fromWrapper, long remainTime, Map<String, WorkerWrapper> forParamUseWrappers) {
+
+    }
+
+    public void work(ExecutorService executorService, long remainTime, Map<String, WorkerWrapper> forParamUseWrappers) {
+        work(executorService, null, remainTime, forParamUseWrappers);
+    }
+
+    public List<WorkerWrapper<?, ?>> getNextWrappers() {
+        return nextWrappers;
+    }
+
+    private int getState() {
+        return state.get();
+    }
+
+    /**
+     * 总控制台超时，停止所有任务
+     */
+    public void stopNow() {
+        if (getState() == WorkerStatusEnum.INIT.getValue()|| getState() == WorkerStatusEnum.WORKING.getValue()) {
+            fastFail(getState(), null);
+        }
+    }
+    private boolean checkIsNullResult() {
+        return ResultState.DEFAULT == workResult.getResultState();
+    }
+
+    private boolean compareAndSetState(int expect, int update) {
+        return this.state.compareAndSet(expect, update);
+    }
+
+    /**
+     * 快速失败
+     */
+    private boolean fastFail(int expect, Exception e) {
+        //试图将它从expect状态,改成Error
+        if (!compareAndSetState(expect, WorkerStatusEnum.ERROR.getValue())) {
+            return false;
+        }
+
+        //尚未处理过结果
+        if (checkIsNullResult()) {
+            if (e == null) {
+                workResult = defaultResult();
+            } else {
+                workResult = defaultExResult(e);
+            }
+        }
+
+        callback.result(false, param, workResult);
+        return true;
+    }
+
+    /**
+     * 默认结果
+     *
+     * @return {@link WorkResult}<{@link V}>
+     */
+    private WorkResult<V> defaultResult() {
+        workResult.setResultState(ResultState.TIMEOUT);
+        workResult.setResult(worker.defaultValue());
+        return workResult;
+    }
+
+    /**
+     * 默认结果(带异常)
+     *
+     * @param ex 前任
+     * @return {@link WorkResult}<{@link V}>
+     */
+    private WorkResult<V> defaultExResult(Exception ex) {
+        workResult.setResultState(ResultState.EXCEPTION);
+        workResult.setResult(worker.defaultValue());
+        workResult.setEx(ex);
+        return workResult;
+    }
 }
