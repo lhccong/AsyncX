38176ab1c57f2e190ca8f2ef4f470cb518d03198
lhccong <771901874@qq.com>
2024-04-28 10:49:52 +0800

    feat: 🚀创建系统时钟 (用于解决高并发下 System.currentTimeMillis 卡顿)

---
+++ src/main/java/com/cong/async/executor/time/SystemClock.java
@@ -0,0 +1,53 @@
+package com.cong.async.executor.time;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * 系统时钟 (用于解决高并发下 System.currentTimeMillis 卡顿)
+ *
+ * @author cong
+ * @date 2024/04/28
+ */
+public class SystemClock {
+
+    private final int period;
+
+    private final AtomicLong now;
+
+    private static class InstanceHolder {
+        private static final SystemClock INSTANCE = new SystemClock(1);
+    }
+
+    public SystemClock(int period) {
+        this.period = period;
+        this.now = new AtomicLong(System.currentTimeMillis());
+        scheduleClockUpdating();
+    }
+
+    private static SystemClock instance() {
+        return InstanceHolder.INSTANCE;
+    }
+
+    private void scheduleClockUpdating() {
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor(runnable -> {
+            Thread thread = new Thread(runnable, "System Clock");
+            thread.setDaemon(true);
+            return thread;
+        });
+        scheduler.scheduleAtFixedRate(() -> now.set(System.currentTimeMillis()), period, period, TimeUnit.MILLISECONDS);
+    }
+
+    private long currentTimeMillis() {
+        return now.get();
+    }
+
+    /**
+     * 用来替换原来的System.currentTimeMillis()
+     */
+    public static long now() {
+        return instance().currentTimeMillis();
+    }
+}
