4a2be6ef59d7e2221c680012e2e0d52b92cb5ee0
lhccong <771901874@qq.com>
2024-04-28 11:01:50 +0800

    feat: 🚀新增执行结果

---
+++ src/main/java/com/cong/async/worker/WorkResult.java
@@ -0,0 +1,73 @@
+package com.cong.async.worker;
+
+/**
+ * 执行结果
+ *
+ * @author cong
+ * @date 2024/04/28
+ */
+public class WorkResult<V> {
+
+    /**
+     * 执行的结果
+     */
+    private V result;
+
+    /**
+     * 结果状态
+     */
+    private ResultState resultState;
+
+    /**
+     * 异常信息
+     */
+    private Exception ex;
+
+    public WorkResult(V result, ResultState resultState) {
+        this(result, resultState, null);
+    }
+
+    public WorkResult(V result, ResultState resultState, Exception ex) {
+        this.result = result;
+        this.resultState = resultState;
+        this.ex = ex;
+    }
+
+    public static <V> WorkResult<V> defaultResult() {
+        return new WorkResult<>(null, ResultState.DEFAULT);
+    }
+
+    @Override
+    public String toString() {
+        return "WorkResult{" +
+                "result=" + result +
+                ", resultState=" + resultState +
+                ", ex=" + ex +
+                '}';
+    }
+
+    public Exception getEx() {
+        return ex;
+    }
+
+    public void setEx(Exception ex) {
+        this.ex = ex;
+    }
+
+    public V getResult() {
+        return result;
+    }
+
+    public void setResult(V result) {
+        this.result = result;
+    }
+
+    public ResultState getResultState() {
+        return resultState;
+    }
+
+    public void setResultState(ResultState resultState) {
+        this.resultState = resultState;
+    }
+
+}
